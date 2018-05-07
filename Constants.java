7d105be9297f7c47f0fee1960e329bfdf2ec1e61
nidhixsharma <nidhi.x.sharma@gmail.com>
2018-05-07 13:56:14 -0700

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class Constants 
+{
+	public static final String URL = "http://expedia.com";
+	public static final String URL1= "https://www.kayak.com/";
+    public static final String File_Path = "D:\\Nidhi\\Workspace\\eclipse\\CheapestFlightRate\\src\\utilities\\";
+    public static final String File_Name = "TestData.xlsx";
+}
